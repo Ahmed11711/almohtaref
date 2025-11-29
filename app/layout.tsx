667374c5ab148789bb41fa63a8133d90fdc3c867
almohtarefsa-coder <almohtarefsa@gmail.com>
2025-11-29 13:28:45 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -135,6 +135,21 @@ export default function RootLayout({
   return (
     <html lang="ar" dir="rtl">
       <head>
+
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-KQXKSZCV');
+            `,
+          }}
+        />
+        {/* End Google Tag Manager */}
+
         {/* Trusted Types Fix */}
         <script
           suppressHydrationWarning
@@ -239,6 +254,18 @@ export default function RootLayout({
       </head>
 
       <body className="antialiased">
+
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-KQXKSZCV"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
+
         <a
           href="#main-content"
           className="sr-only focus:not-sr-only focus:absolute focus:top-4 focus:left-4 focus:z-[100] focus:px-4 focus:py-2 focus:bg-yellow-400 focus:text-black focus:font-bold focus:rounded focus:shadow-lg"
