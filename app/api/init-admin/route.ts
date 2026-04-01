61fda1487fd16addf05097155173987160beccad
Ahmed11711 <ahmed11711@gmail.com>
2026-04-01 21:09:47 +0200

    init

---
+++ app/api/init-admin/route.ts
@@ -0,0 +1,10 @@
+import { NextResponse } from 'next/server';
+import connectDB from '@/lib/mongodb';
+import Admin from '@/models/Admin';
+
+export async function GET() {
+  await connectDB();
+  // الموديل بيشفر الباسورد أوتوماتيك لو استخدمت .save() أو .create()
+  await Admin.create({ username: 'admin', password: 'admin123' });
+  return NextResponse.json({ message: "Admin Created and Hashed!" });
+}
